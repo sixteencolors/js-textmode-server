58016578b8529d4314a7d0e8ce8214d29e898a99
Doug Moore <lordscarlet@idledreams.net>
2012-07-24 19:53:14 -0400

    Basic proof of concept code

---
+++ app.js
@@ -0,0 +1,23 @@
+var io = require('socket.io').listen(8000);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('set nickname', function (name) {
+    socket.set('nickname', name, function () { 
+      socket.nickname = name;
+      socket.emit('ready'); 
+    });
+  });
+
+  socket.on('msg', function () {
+    socket.get('nickname', function (err, name) {
+      console.log('Chat message by ', name);
+    });
+  });
+
+  socket.on('putChar', function(data) {
+    console.log('character data received from ' + socket.nickname + ': ' + data);
+    socket.broadcast.emit('putChar', socket.nickname, data)
+  })
+});
+
+
