3e681d1545ddcde693674f927bc144d14a0b3af6
Doug Moore <lordscarlet@idledreams.net>
2012-08-03 19:15:21 -0400

    Setting some groundwork

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-var io = require('socket.io').listen(8000);
+var express = require('express');
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+// routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+var usernames = {};
 
 io.sockets.on('connection', function (socket) {
-  socket.on('set nickname', function (name) {
+  socket.on('sendchat', function(data) {
+    io.sockets.emit('updatechat', socket.username, data);
+  })
+
+  socket.on('adduser', function(username) {
+    socket.username = username;
+    usernames[username] = username;
+    socket.emit('updatechat', 'SERVER', 'you have connected');
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+    io.sockets.emit('updateusers', usernames);
+  })
+
+  socket.on('disconnect', function() {
+    delete usernames[socket.username];
+    io.sockets.emit('udpateusers', usernames);
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+  });
+
+  /* socket.on('set nickname', function (name) {
     socket.set('nickname', name, function () { 
       socket.nickname = name;
       socket.emit('ready'); 
     });
-  });
+  }); 
 
   socket.on('msg', function () {
     socket.get('nickname', function (err, name) {
       console.log('Chat message by ', name);
       socket.broadcast.emit('join', socket.nickname);
     });
-  });
+  }); */
 
   socket.on('putChar', function(data) {
     console.log('character data received from ' + socket.nickname + ': ' + data);
---
+++ package.js
@@ -0,0 +1,9 @@
+{
+	"name": "js-textmode-server",
+	"description": "Server to facilitate multi-user editing in js-textmode-editor",
+	"version": "0.0.1",
+	"dependencies": {
+		"express": "3.0.0rc1",
+		"socket.io": "0.9.9"
+	}
+}
